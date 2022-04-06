2f5d03859f8341e023aad6f28832f26ec33ee3e4
FrancielledeAbreu <francielledeabreus@gmail.com>
2022-04-06 17:54:10 +0100

    feat: Card component created

---
+++ cnab-front/src/components/Card/Card.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  border: 1px solid #ccc;
+`;
+
+export const Title = styled.div`
+  border-bottom: 1px solid #ccc;
+  color: #726868;
+  padding: 1%;
+  font-weight: 900;
+`;
+
+export const Body = styled.div`
+  color: #726868;
+  padding: 1%;
+`;
